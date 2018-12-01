c9cd418a935eb0d78e7f8cf062eccb73e73467b2
Manish Lad <manish@lad.eu.com>
2018-12-01 18:31:24 +0000

    Complete Chronal Calibration
    Update to find first repeated frequency

--- 2018/01/calibrate.py
+++ 2018/01/calibrate.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python
 
-def calibrate(in_file, curr_freq=0):
+def get_freq(in_file):
+    while True:
+        with open(in_file, 'r') as fin:
+            for line in fin:
+                yield int(line)
+
+def find_first_repeat(in_file, curr_freq=0):
+    freqs = set()
+    freqs.add(curr_freq)
+    for item in get_freq(in_file):
+        curr_freq += item
+        if curr_freq in freqs:
+            return curr_freq
+        else:
+            freqs.add(curr_freq)
+
+def calibrate_first_pass(in_file, curr_freq=0):
     with open(in_file, 'r') as fin:
         for line in fin:
             freq_change = int(line)
@@ -8,8 +24,11 @@ def calibrate(in_file, curr_freq=0):
     return curr_freq
 
 def main():
-    result = calibrate("input")
-    print('Resulting Frequency = ', result)
+    result = calibrate_first_pass("input")
+    print('Resulting frequency on first pass = ', result)
+
+    first_repeat = find_first_repeat("input")
+    print('First repeated frequency = ', first_repeat)
 
 if __name__ == '__main__':
     main()
