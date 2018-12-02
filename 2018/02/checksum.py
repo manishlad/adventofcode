06fdf6ba95917c8778538c55f517e2770b9405ea
Manish Lad <manish@lad.eu.com>
2018-12-02 15:01:03 +0000

    Find pair of boxes with common chars

--- 2018/02/checksum.py
+++ 2018/02/checksum.py
@@ -17,9 +17,25 @@ def calculate_checksum(in_file):
     checksum = doubles * triples
     return checksum
 
+def get_common_chars(in_file):
+    checksum_list = open(in_file, 'r').readlines()
+    checksum_length = len(checksum_list[0])
+    checksum_pairs = [(checksum_list[i], checksum_list[j])
+                      for i in range(len(checksum_list))
+                      for j in range(i+1, len(checksum_list))
+                      if i != j]
+    for pair in checksum_pairs:
+        match_chars = [i for i, j in zip(pair[0], pair[1]) if i == j]
+        if len(match_chars) == checksum_length - 1:
+            return ''.join(match_chars)
+
+
 def main(box_id_list):
     checksum = calculate_checksum(box_id_list)
     print("Checksum = ", checksum)
+
+    common_chars = get_common_chars(box_id_list)
+    print("Common characters are ", common_chars)
 
 if __name__ == '__main__':
     input_file = "input.txt"
