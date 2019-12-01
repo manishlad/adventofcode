60e6d89220f0f9eaaf908b140f46d9e1e2918aea
Manish Lad <manish@lad.eu.com>
2019-12-01 14:35:16 +0000

    2019-12-01 Part Two
    Calculate adjusted total fuel required

--- 2019/01/fuel_req.py
+++ 2019/01/fuel_req.py
@@ -3,9 +3,17 @@
 def calculate_fuel_req(mass):
     return ((mass // 3) - 2)
 
+def calculate_fuel_req_recursively(mass):
+    fuel_req = calculate_fuel_req(mass)
+    if fuel_req <= 0:
+        fuel_req = 0
+    else:
+        fuel_req = fuel_req + calculate_fuel_req_recursively(fuel_req)
+    return fuel_req
+
 def main(module_masses_file):
     with open(module_masses_file, 'r') as module_masses:
-        fuel_reqs = [ calculate_fuel_req(int(mass)) for mass in module_masses ]
+        fuel_reqs = [ calculate_fuel_req_recursively(int(mass)) for mass in module_masses ]
     total_fuel_req = sum(fuel_reqs)
     print("Total fuel required is:", total_fuel_req)
 
--- 2019/01/fuel_req_test.py
+++ 2019/01/fuel_req_test.py
@@ -12,6 +12,17 @@ class FuelReqTests(unittest.TestCase):
         self.assertEqual(fuel_req.calculate_fuel_req(1969), 654)
         self.assertEqual(fuel_req.calculate_fuel_req(100756), 33583)
 
+    def test_calculate_fuel_req_recursively(self):
+        self.assertEqual(
+            fuel_req.calculate_fuel_req_recursively(14),
+            2)
+        self.assertEqual(
+            fuel_req.calculate_fuel_req_recursively(1969),
+            (654 + 216 + 70 + 21 + 5))
+        self.assertEqual(
+            fuel_req.calculate_fuel_req_recursively(100756),
+            (33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2))
+
 
 if __name__ == '__main__':
     unittest.main()
