8f29e71f45c16b18f87c57b0c7b8072a15f859d6
Manish Lad <manishlad@users.noreply.github.com>
2020-12-22 18:17:14 +0000

    Count number of valid passwords with modified policy

--- 2020/02/password_philosophy.py
+++ 2020/02/password_philosophy.py
@@ -9,15 +9,26 @@ def validate(password, policy):
         return True
     return False
 
+def validate2(password, policy):
+    p1, p2, char = re.split(' |-', policy)
+    positions = password[int(p1)-1] + password[int(p2)-1]
+    if positions.count(char) == 1:
+        return True
+    return False
+
 
 def main(passwords_file):
     valid_passwords = []
+    valid_passwords2 = []
     with open(passwords_file, 'r') as passwords:
         for p in passwords:
             pol, pwd = p.strip().split(': ')
             if validate(pwd, pol):
                 valid_passwords.append(p)
+            if validate2(pwd, pol):
+                valid_passwords2.append(p)
     print(len(valid_passwords), 'valid passwords found')
+    print(len(valid_passwords2), 'valid passwords found')
 
 
 if __name__ == '__main__':
