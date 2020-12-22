853525c43c0bb4dcbd1eb41e637676178d4d510f
Manish Lad <manishlad@users.noreply.github.com>
2020-12-22 22:21:43 +0000

    Multiply all trees on all slopes

--- 2020/03/toboggan_trajectory.py
+++ 2020/03/toboggan_trajectory.py
@@ -1,16 +1,38 @@
 #!/usr/bin/env python3
 
-def main(map_file):
+from math import prod
+
+
+slopes = [
+    (1, 1),
+    (3, 1),
+    (5, 1),
+    (7, 1),
+    (1, 2)
+]
+
+
+def traverse(map_file, coords):
     position = 0
+    down = 1
     path = []
     with open(map_file, 'r') as treemap:
         for line in treemap:
-            path.append(line.strip()[position % len(line.strip())])
-            print(line.strip()[position % len(line.strip())])
-            position = position + 3
+            if down > 1:
+                down = down - 1
+            else:
+                path.append(line.strip()[position % len(line.strip())])
+                position = position + coords[0]
+                down = coords[1]
     num_trees = path.count('#')
-    print('There are', num_trees, 'along the path')
+    return num_trees
 
+
+def main(map_file):
+    part1 = traverse(map_file, (3, 1))
+    print("There are", part1, "trees in slope 1")
+    part2 = prod([traverse(map_file, coords) for coords in slopes])
+    print("Product of number of trees encountered on each slope =", part2)
 
 if __name__ == '__main__':
     map_file = 'input.dat'
