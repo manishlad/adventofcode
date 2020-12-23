88817f4490c42ecd082a49f29d3c5f70685232d6
Manish Lad <manishlad@users.noreply.github.com>
2020-12-23 13:13:17 +0000

    Count number of valid passports with validated fields

--- 2020/04/passport_processing.py
+++ 2020/04/passport_processing.py
@@ -1,6 +1,62 @@
 #!/usr/bin/env python3
 
+import re
+
+
 p_keys = {'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'}
+
+
+def in_range(value, minimum, maximum):
+    if minimum <= value <= maximum:
+        return True
+    return False
+
+
+def validate_byr(value):
+    return in_range(int(value), 1920, 2002)
+
+
+def validate_iyr(value):
+    return in_range(int(value), 2010, 2020)
+
+
+def validate_eyr(value):
+    return in_range(int(value), 2020, 2030)
+
+
+def validate_hgt(value):
+    if value[-2:] == 'cm':
+        return in_range(int(value[:-2]), 150, 193)
+    elif value[-2:] == 'in':
+        return in_range(int(value[:-2]), 59, 76)
+    return False
+
+
+def validate_hcl(value):
+    if re.fullmatch("#[0-9a-z]{6}", value):
+        return True
+
+
+def validate_ecl(value):
+    return value in {'amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'}
+
+
+def validate_pid(value):
+    return len(value) == 9 and value.isdigit()
+
+
+def validate_fields(passport):
+    if (
+        validate_byr(passport['byr']) and
+        validate_iyr(passport['iyr']) and
+        validate_eyr(passport['eyr']) and
+        validate_hgt(passport['hgt']) and
+        validate_hcl(passport['hcl']) and
+        validate_ecl(passport['ecl']) and
+        validate_pid(passport['pid'])
+    ):
+        return True
+    return False
 
 
 def validate(passports):
@@ -25,9 +81,12 @@ def main(input_file):
                 passports.append(d)
                 d = {}
         passports.append(d)  # remember to add the last entry
-    print(len(passports))
     valid_passports = validate(passports)
-    print('There are', len(valid_passports), 'valid passports')
+    print('There are', len(valid_passports), 'supposedly valid passports')
+
+    valid_passports = [item
+                       for item in valid_passports if validate_fields(item)]
+    print('There are', len(valid_passports), 'actually valid passports')
 
 
 if __name__ == '__main__':
