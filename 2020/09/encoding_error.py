f861781484be8c40328b6c173e5b582219314983
Manish Lad <manishlad@users.noreply.github.com>
2020-12-26 18:15:45 +0000

    Find first the XMAS encryption weakness

--- 2020/09/encoding_error.py
+++ 2020/09/encoding_error.py
@@ -18,21 +18,36 @@ def check(n, prev25):
     return False
 
 
-def main(input_file):
-    xmas_code = xmas(input_file)
-    prev25 = []
+def find_invalid(xmas_code):
+    prev = []
+    invalid = None
     for item in xmas_code:
         c, n = item
         if c >= 25:
-            valid = check(n, prev25)
-            if valid:
-                prev25 = prev25[1:] + [n]
-            else:
-                print('Invlid entry at', c+1, ':', n)
-                return
-        else:
-            prev25.append(n)
-    print('All valid')
+            valid = check(n, prev[-25:])
+            if not valid:
+                invalid = n
+                return prev, c, invalid
+        prev.append(n)
+    return prev, c, None
+
+
+def contig_combos(src_list):
+    for i in range(0, len(src_list)):
+        for j in range(i+2, len(src_list)):
+            yield src_list[i:j]
+
+
+def main(input_file):
+    xmas_code = xmas(input_file)
+    prev, position, invalid = find_invalid(xmas_code)
+    if invalid is not None:
+        print('Invalid entry at', position+1, ':', invalid)
+
+        for combo in contig_combos(prev):
+            if sum(combo) == invalid:
+                print('Encryption weakness =', min(combo) + max(combo),
+                      'because of:', min(combo), max(combo), combo)
 
 
 if __name__ == '__main__':
