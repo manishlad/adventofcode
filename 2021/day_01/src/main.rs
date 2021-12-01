fabc3505e5b4ea275822d723df698b1878df88da
Manish Lad <manishlad@users.noreply.github.com>
2021-12-01 22:23:46 +0000

    Day 1 part 1

---
+++ 2021/day_01/src/main.rs
@@ -0,0 +1,39 @@
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+
+fn count_increases(depth_list: Vec<i32>){
+    let mut counter = 0;
+    let mut num_increased: i32 = 0;
+    while counter < depth_list.len()-1 {
+        if depth_list[counter] < depth_list[counter+1] {
+            num_increased += 1;
+        }
+        counter += 1;
+    }
+    println!("Number of depth increases: {}", num_increased);    
+}
+
+fn main() {
+    println!("Let's begin Advent of Code!");
+    // let input_dat: Vec<i32> = Vec::from([199,
+    //     200,
+    //     208,
+    //     210,
+    //     200,
+    //     207,
+    //     240,
+    //     269,
+    //     260,
+    //     263]);
+
+    let mut file = File::open("input.dat").expect("Error reading file!");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("Oops cannot reaf file");
+    let mut input_dat: Vec<i32> = Vec::new();
+    for s in contents.lines() {
+        input_dat.push(s.parse::<i32>().unwrap());
+    }
+
+    count_increases(input_dat);
+}
