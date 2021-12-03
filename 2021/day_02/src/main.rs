ae51d18dc8c9968d087480abd0a7367b4f994d6c
Manish Lad <manishlad@users.noreply.github.com>
2021-12-03 19:40:26 +0000

    Day 2 part 1

---
+++ 2021/day_02/src/main.rs
@@ -0,0 +1,53 @@
+fn main() {
+    println!("Advent of Code - Day 2!");
+
+    let course = get_input();
+    let cmd_list = get_cmd_list(course);
+
+    let mut position = (0,0); // h_pos, depth
+
+    navigate(&mut position, cmd_list);
+
+    println!("Position = {:?}\nMultiplier = {}", position, position.0 * position.1);
+}
+
+fn navigate(pos: &mut (i32, i32), cmd_list: Vec<(String, i32)>) {
+    for cmd in cmd_list.iter() {
+        match cmd.0.as_str() {
+            "forward" => pos.0 += cmd.1,
+            "down" => pos.1 += cmd.1,
+            "up" => pos.1 -= cmd.1,
+            &_ => print!("error"),
+          }
+    }
+}
+
+fn get_cmd_list(commands: Vec<String>) -> Vec<(String, i32)> {
+    let mut cmd_list: Vec<(String, i32)> = Vec::new();
+    for cmd in commands.iter() {
+        let c: Vec<_> = cmd.split(' ').collect();
+        cmd_list.push((String::from(c[0]), c[1].parse::<i32>().unwrap()));
+    }
+    cmd_list
+}
+
+fn get_test_input() -> Vec<String> {
+    let input: Vec<String> = vec![
+        "forward 5",
+        "down 5",
+        "forward 8",
+        "up 3",
+        "down 8",
+        "forward 2"
+        ].iter().map(|&s| s.into()).collect();
+    input
+}
+
+fn get_input() -> Vec<String> {
+    let commands = std::fs::read_to_string("input.dat").expect("Error reading input file!");
+    let mut input_dat: Vec<String> = Vec::new();
+    for command in commands.lines() {
+        input_dat.push(String::from(command));
+    }
+    input_dat
+}
