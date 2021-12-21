2dde40c74b9052a0b2ec866d38a8bcf8573af184
Manish Lad <manishlad@users.noreply.github.com>
2021-12-21 17:12:03 +0000

    Day 5 part 2

--- 2021/day_05/src/main.rs
+++ 2021/day_05/src/main.rs
@@ -1,21 +1,34 @@
-use std::cmp::min;
 use std::cmp::max;
+use std::cmp::min;
+use std::iter::Iterator;
 
 fn main() {
     println!("Advent of Code - Day 5!");
 
     // let input: &str = "test_input.dat";
-    // let mut ocean_floor: Vec<Vec<u16>> = vec![vec![0; 10]; 10];
     let input: &str = "input.dat";
+
+    // let mut ocean_floor: Vec<Vec<u16>> = vec![vec![0; 10]; 10];
     let mut ocean_floor: Vec<Vec<u16>> = vec![vec![0; 1000]; 1000];
+    ocean_floor = read_input(input, ocean_floor, false);
+    let mut overlaps: u16 = count_overlaps(ocean_floor);
+    println!("Part 1: Number of overlaps = {}", overlaps);
 
-    ocean_floor = read_input(input, ocean_floor);
-
-    let overlaps: u16 = count_overlaps(ocean_floor);
-    println!("Number of overlaps = {}", overlaps);
+    // ocean_floor = vec![vec![0; 10]; 10];
+    ocean_floor = vec![vec![0; 1000]; 1000];
+    ocean_floor = read_input(input, ocean_floor, true);
+    overlaps = count_overlaps(ocean_floor);
+    println!(
+        "Part 2: Number of overlaps including diagonals = {}",
+        overlaps
+    );
 }
 
-fn read_input(in_file: &str, mut ocean_floor: Vec<Vec<u16>>) -> Vec<Vec<u16>> {
+fn read_input(
+    in_file: &str,
+    mut ocean_floor: Vec<Vec<u16>>,
+    include_diagonals: bool,
+) -> Vec<Vec<u16>> {
     let input = std::fs::read_to_string(in_file).expect("Error reading input file!");
 
     for vertices in input.lines() {
@@ -31,12 +44,37 @@ fn read_input(in_file: &str, mut ocean_floor: Vec<Vec<u16>>) -> Vec<Vec<u16>> {
             .collect::<Vec<usize>>();
 
         if start_xy[0] == end_xy[0] {
-            for y in min(start_xy[1], end_xy[1])..max(start_xy[1], end_xy[1])+1 {
+            for y in min(start_xy[1], end_xy[1])..=max(start_xy[1], end_xy[1]) {
                 ocean_floor[y][start_xy[0]] += 1;
             }
         } else if start_xy[1] == end_xy[1] {
-            for x in min(start_xy[0], end_xy[0])..max(start_xy[0], end_xy[0])+1 {
+            for x in min(start_xy[0], end_xy[0])..=max(start_xy[0], end_xy[0]) {
                 ocean_floor[start_xy[1]][x] += 1;
+            }
+        } else if include_diagonals {
+            let x_range: Box<dyn Iterator<Item = usize>>;
+            let y_range: Box<dyn Iterator<Item = usize>>;
+            if (start_xy[0] < end_xy[0]) && (start_xy[1] < end_xy[1]) {
+                x_range = Box::new(start_xy[0]..=end_xy[0]) as Box<dyn Iterator<Item = usize>>;
+                y_range = Box::new(start_xy[1]..=end_xy[1]) as Box<dyn Iterator<Item = usize>>;
+            } else if (start_xy[0] < end_xy[0]) && (start_xy[1] > end_xy[1]) {
+                x_range = Box::new(start_xy[0]..=end_xy[0]) as Box<dyn Iterator<Item = usize>>;
+                y_range =
+                    Box::new((end_xy[1]..=start_xy[1]).rev()) as Box<dyn Iterator<Item = usize>>;
+            } else if (start_xy[0] > end_xy[0]) && (start_xy[1] < end_xy[1]) {
+                x_range =
+                    Box::new((end_xy[0]..=start_xy[0]).rev()) as Box<dyn Iterator<Item = usize>>;
+                y_range = Box::new(start_xy[1]..=end_xy[1]) as Box<dyn Iterator<Item = usize>>;
+            } else {
+                // if (start_xy[0] > end_xy[0]) && (start_xy[1] > end_xy[1])
+                x_range =
+                    Box::new((end_xy[0]..=start_xy[0]).rev()) as Box<dyn Iterator<Item = usize>>;
+                y_range =
+                    Box::new((end_xy[1]..=start_xy[1]).rev()) as Box<dyn Iterator<Item = usize>>;
+            }
+            let coords = x_range.zip(y_range);
+            for c in coords {
+                ocean_floor[c.1][c.0] += 1;
             }
         }
     }
