f0c12f4b9abb0f9e7f51645544dae5d0f0455fd1
Manish Lad <manishlad@users.noreply.github.com>
2022-12-03 16:18:06 +0000

    2022 Day 2 part 2

--- 2022/day_02/src/main.rs
+++ 2022/day_02/src/main.rs
@@ -5,7 +5,8 @@ static TESTING: bool = false;
 fn main() {
     println!("Advent of Code 2022 - Day 02!");
 
-    let rps_map2: HashMap<&str, i32> = HashMap::from([
+    // Part 1
+    let rps_map: HashMap<&str, i32> = HashMap::from([
         ("A X", 1+3),
         ("A Y", 2+6),
         ("A Z", 3+0),
@@ -18,8 +19,24 @@ fn main() {
     ]);
 
     let given_strategy: String = get_input();
-    let given_strategy_score: i32 = given_strategy.lines().fold(0, |acc, round| acc + rps_map2.get(round).unwrap());
+    let given_strategy_score: i32 = given_strategy.lines().fold(0, |acc, round| acc + rps_map.get(round).unwrap());
     println!("Total score with given strategy is: {}", given_strategy_score);
+
+    // Part 2
+    let rps_map2: HashMap<&str, i32> = HashMap::from([
+        ("A X", 3+0),
+        ("A Y", 1+3),
+        ("A Z", 2+6),
+        ("B X", 1+0),
+        ("B Y", 2+3),
+        ("B Z", 3+6),
+        ("C X", 2+0),
+        ("C Y", 3+3),
+        ("C Z", 1+6),
+    ]);
+
+    let correct_strategy_score: i32 = given_strategy.lines().fold(0, |acc, round| acc + rps_map2.get(round).unwrap());
+    println!("Total score with correct strategy decoding is: {}", correct_strategy_score);
 }
 
 fn get_input() -> String {
