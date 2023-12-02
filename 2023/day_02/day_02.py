e7b9b87ef670874dba7f293c8e79ddd8cd5ac9ca
Manish Lad <manishlad@users.noreply.github.com>
2023-12-02 16:39:41 +0000

    2023 Python Day 2 part 2

--- 2023/day_02/day_02.py
+++ 2023/day_02/day_02.py
@@ -16,6 +16,7 @@ def usage():
 
 def main(input_file):
     possible_games = []
+    game_powers = []
     with open(input_file, 'r') as games:
         for game in games:
             gID, subsets = game.split(':')
@@ -23,17 +24,19 @@ def main(input_file):
             reds = re.findall(r'\d+ red', subsets)
             max_red = max(
                 [int(re.search(r'\d+', x).group()) for x in reds])
-            blues = re.findall(r'\d+ blue', subsets)
-            max_blue = max(
-                [int(re.search(r'\d+', x).group()) for x in blues])
             greens = re.findall(r'\d+ green', subsets)
             max_green = max(
                 [int(re.search(r'\d+', x).group()) for x in greens])
+            blues = re.findall(r'\d+ blue', subsets)
+            max_blue = max(
+                [int(re.search(r'\d+', x).group()) for x in blues])
+            game_powers.append(max_red * max_green * max_blue)
             if (max_red <= MAX_CUBES['red']
-                    and max_blue <= MAX_CUBES['blue']
-                    and max_green <= MAX_CUBES['green']):
+                    and max_green <= MAX_CUBES['green']
+                    and max_blue <= MAX_CUBES['blue']):
                 possible_games.append(gameID)
     print("Sum of IDs of possible games:", sum(possible_games))
+    print("Sum of IDs of power of game sets:", sum(game_powers))
 
 
 if __name__ == "__main__":
