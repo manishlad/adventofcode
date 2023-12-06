ca65f446c72a8d5cc55965b27d07b861e985de3a
Manish Lad <manishlad@users.noreply.github.com>
2023-12-06 22:32:44 +0000

    2023 Python Day 6 part 2

--- 2023/day_06/day_06.py
+++ 2023/day_06/day_06.py
@@ -18,13 +18,22 @@ Distance:   347   1522   1406   1471
 
 
 def parse_input(input):
-    races = [re.split(r':', td)[1].split() for td in input.strip().split('\n')]
+    races = [re.split(r":", td)[1].split() for td in input.strip().split("\n")]
     races = [map(int, value) for value in races]
     return list(zip(races[0], races[1]))
 
 
+def parse_input_kerned(input):
+    races = [
+        re.split(r":", td)[1].replace(" ", "")
+        for td in input.strip().split("\n")
+    ]
+    races = tuple(map(int, races))
+    return [races]
+
+
 def push_ratios(time):
-    ratios = list(zip(range(1, time + 1), reversed(range(0, time))))
+    ratios = zip(range(1, time + 1), reversed(range(0, time)))
     return ratios
 
 
@@ -34,19 +43,52 @@ def calculate_wins(ratios, record):
     return winners
 
 
-def main():
+def calculate_wins2(ratios, record):
+    winners = list(filter(lambda r: r[0] * r[1] > record, ratios))
+    return winners
+#     for r in ratios:
+#         if r[0] * r[1] > record:
+#             lower = r[0]
+#             break
+#     for r in reversed(ratios):
+#         if r[0] * r[1] > record:
+#             upper = r[0]
+#             break
+#     return (lower, upper)
+
+
+def find_margins(races):
     margins = []
-    if TEST:
-        races = parse_input(TEST_INPUT)
-    else:
-        races = parse_input(PUZZLE_INPUT)
     for race in races:
         ratios = push_ratios(race[0])
         win_combos = calculate_wins(ratios, race[1])
         margins.append(len(win_combos))
+    return margins
 
+
+def find_margins2(races):
+    for race in races:
+        ratios = push_ratios(race[0])
+        winners = calculate_wins2(ratios, race[1])
+        lower, upper = winners[0]
+    return (upper + 1) - lower
+
+
+def main():
+    if TEST:
+        races = parse_input(TEST_INPUT)
+    else:
+        races = parse_input(PUZZLE_INPUT)
+    margins = find_margins(races)
     margins_product = reduce(lambda x, y: x * y, margins)
     print("Part 1:", margins_product)
+
+    if TEST:
+        races = parse_input_kerned(TEST_INPUT)
+    else:
+        races = parse_input_kerned(PUZZLE_INPUT)
+    margins = find_margins2(races)
+    print("Part 2:", margins)
 
 
 if __name__ == "__main__":
